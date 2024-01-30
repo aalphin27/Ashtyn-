07610c907feaf1669903eb092184123c199ab3f8
Ashtyn Alphin <ashtynalphin@Ashtyns-MacBook-Pro.local>
2024-01-30 18:16:21 -0500

    Added First Program

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+/**
+ *        @file: hello.cpp
+ *      @author: Ashtyn Alphin
+ *        @date: January 30, 2024
+ *       @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///function prototypes
+
+int main(int argc, char const *argv[]) {
+
+   cout << "Hello World" << endl;
+
+   return 0;
+}// main
